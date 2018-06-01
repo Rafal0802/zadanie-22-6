9edca00b0fa219ecc7f93df81ee7a671e7a07196
Rafal Swieton <rafal@eskrzynka.pl>
2018-06-01 17:39:36 +0200

    Add and change files

--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -4,6 +4,7 @@ import callApi from '../../util/apiCaller';
 export const ADD_POST = 'ADD_POST';
 export const ADD_POSTS = 'ADD_POSTS';
 export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
 
 // Export Actions
 export function addPost(post) {
